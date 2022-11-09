a3755e6d0fb8ff4f0192ce5ea7988ba6980be797
Local Administrator <hieu.lh15@outlook.com.vn>
2022-11-09 15:14:42 +0700

    modify name

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout2);
+        setContentView(R.layout.task_2_layout);
     }
 }
